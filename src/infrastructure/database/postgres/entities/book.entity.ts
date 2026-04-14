e8d1fd0f09cca93c98280f2a155ddf32e01d0bdd
Agustin-Perezz <agustinnicolas806@gmail.com>
2026-04-14 09:51:34 -0300

    Add Book domain and database entities

---
+++ src/infrastructure/database/postgres/entities/book.entity.ts
@@ -0,0 +1,37 @@
+import { Entity, PrimaryKey, Property, Index } from '@mikro-orm/postgresql';
+import { v7 as uuidv7 } from 'uuid';
+
+@Entity({ tableName: 'books' })
+export class BookEntity {
+  @PrimaryKey()
+  id: string = uuidv7();
+
+  @Index()
+  @Property()
+  title: string;
+
+  @Property()
+  author: string;
+
+  @Property({ unique: true })
+  isbn: string;
+
+  @Property()
+  publicationYear: number;
+
+  @Property({ nullable: true })
+  genre: string | null = null;
+
+  @Property()
+  createdAt: Date = new Date();
+
+  @Property({ onUpdate: () => new Date() })
+  updatedAt: Date = new Date();
+
+  constructor(title: string, author: string, isbn: string, publicationYear: number) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+    this.publicationYear = publicationYear;
+  }
+}
